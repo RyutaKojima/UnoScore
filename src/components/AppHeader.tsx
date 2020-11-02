9743689c84a510e0e0ebfceaf4c5dc48dc5025e1
miyamae <yu1ro.mya@gmail.com>
2020-11-02 17:44:40 +0900

    update: AppHeader

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import clsx from 'clsx'
+import Image from 'next/image'
 
 type Props = {
   className?: string
@@ -7,8 +8,11 @@ type Props = {
 
 export const AppHeader: React.FC<Props> = ({ className }) => {
   return (
-    <header className={clsx('p-4 border-b', className)}>
-      <h1 className="text-center">UNO Score</h1>
+    <header className={clsx('p-4', className)}>
+      <div className="flex justify-center items-center space-x-4">
+        <Image src="/img/draw4.png" width={25} height={40} className="shadow" />
+        <h1 className="text-2xl font-bold italic text-gray-800">UNO Score</h1>
+      </div>
     </header>
   )
 }
