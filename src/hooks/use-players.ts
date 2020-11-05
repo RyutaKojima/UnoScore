afce79d2d50bde3635d47648a45f30cf71b5fd78
miyamae <yu1ro.mya@gmail.com>
2020-11-05 23:01:10 +0900

    update: usePlayers ストアとつなげる

--- src/hooks/use-players.ts
+++ src/hooks/use-players.ts
@@ -1,5 +1,13 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootStateType } from '../store'
+import { gameSlice } from '../store/game'
 
-export const usePlayers = () => {
-  return useState<string[]>([])
+export const usePlayers = (): [string[], (players: string[]) => void] => {
+  const players = useSelector((state: RootStateType) => state.game.players)
+
+  const dispatch = useDispatch()
+  const setPlayers = (players: string[]) =>
+    dispatch(gameSlice.actions.setPlayers(players))
+
+  return [players, setPlayers]
 }
