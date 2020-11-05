5ecb41f02e19cf1dd9ab7c911591fca316d76cea
miyamae <yu1ro.mya@gmail.com>
2020-11-05 22:58:09 +0900

    update: useRounds ストアとつなげる

--- src/hooks/use-rounds.ts
+++ src/hooks/use-rounds.ts
@@ -1,6 +1,14 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootStateType } from '../store'
+import { gameSlice } from '../store/game'
 import { IRound } from '../interfaces/round'
 
-export const useRounds = () => {
-  return useState<IRound[]>([])
+export const useRounds = (): [IRound[], (rounds: IRound[]) => void] => {
+  const rounds = useSelector((state: RootStateType) => state.game.rounds)
+
+  const dispatch = useDispatch()
+  const setRounds = (rounds: IRound[]) =>
+    dispatch(gameSlice.actions.setRounds(rounds))
+
+  return [rounds, setRounds]
 }
