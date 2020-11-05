a75e7ecc1b2da9e0a855fd20177a91fb9c9cf45b
miyamae <yu1ro.mya@gmail.com>
2020-11-05 16:26:53 +0900

    update: 合計に倍率反映前も表示したい

---
+++ src/components/ResultTableFooter.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { TableCell } from './TableCell'
+import { sumArray } from '../utils/utils'
+import { IResult } from '../interfaces/result'
+
+type Props = {
+  results: IResult[][]
+  magnification: number
+}
+
+export const ResultTableFooter: React.FC<Props> = ({
+  results,
+  magnification,
+}) => {
+  const totalScores: number[] = results.reduce(
+    (prev: number[], result): number[] => {
+      const scores: number[] = result.map((r) => r.score)
+      if (prev.length === 0) {
+        return scores
+      }
+
+      return sumArray(prev, scores)
+    },
+    []
+  )
+
+  const finalScores: number[] = totalScores.map(
+    (score) => score * magnification
+  )
+
+  return (
+    <tfoot>
+      <tr className="bg-gray-100">
+        <th className="border">
+          <span className="text-xs font-bold text-gray-700">合計</span>
+        </th>
+        {totalScores.map((score, index) => (
+          <TableCell
+            key={`result-score-${index}`}
+            className="text-center border"
+          >
+            <span className="font-bold text-gray-800">{score}</span>
+          </TableCell>
+        ))}
+      </tr>
+      <tr>
+        <th className="border">
+          <span className="text-xs font-bold text-gray-700">
+            合計(x{magnification})
+          </span>
+        </th>
+        {finalScores.map((score, index) => (
+          <TableCell key={`final-${index}`} className="text-center border">
+            <span className="font-bold text-gray-800">{score}</span>
+          </TableCell>
+        ))}
+      </tr>
+    </tfoot>
+  )
+}
