fddedf059cc0217bc76ba662ee50f7c866d8251c
miyamae <yu1ro.mya@gmail.com>
2020-11-11 16:20:41 +0900

    remove: redux

--- src/test/pages/index.test.tsx
+++ src/test/pages/index.test.tsx
@@ -2,17 +2,10 @@ import React from 'react'
 import { render } from '../testUtils'
 // import { render, fireEvent } from '../testUtils'
 import { Home } from '../../pages/index'
-import { Provider } from 'react-redux'
-import { store } from '../../store'
-//
+
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(
-      <Provider store={store}>
-        <Home />
-      </Provider>,
-      {}
-    )
+    const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
   //   it('clicking button triggers alert', () => {
