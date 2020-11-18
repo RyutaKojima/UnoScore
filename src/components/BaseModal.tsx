460ead0650302a07efa8cac1bed9c309898f848c
miyamae <yu1ro.mya@gmail.com>
2020-11-18 17:41:03 +0900

    update: モーダル表示時にbodyのスクロールを止める

--- src/components/BaseModal.tsx
+++ src/components/BaseModal.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import clsx from 'clsx'
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock'
 
 type Props = {
   isShow: boolean
@@ -8,12 +13,29 @@ type Props = {
 }
 
 export const BaseModal: React.FC<Props> = ({ isShow, className, children }) => {
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!modalRef.current) return
+
+    if (isShow) {
+      disableBodyScroll(modalRef.current)
+    } else {
+      enableBodyScroll(modalRef.current)
+    }
+
+    return () => {
+      clearAllBodyScrollLocks()
+    }
+  }, [isShow])
+
   const container = process.browser ? document.querySelector('#__next') : null
 
   return container && isShow
     ? ReactDOM.createPortal(
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4">
           <div
+            ref={modalRef}
             className={clsx(
               'bg-white p-4 max-h-90vh w-full max-w-lg rounded-lg relative overflow-y-scroll',
               className
