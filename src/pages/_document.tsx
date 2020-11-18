078c9d884597b2a7aa587d21ba53983bcd824c85
miyamae <yu1ro.mya@gmail.com>
2020-11-18 16:25:48 +0900

    update: icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,12 +16,33 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="manifest.json" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/icons/apple-touch-icon.png"
           />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="apple-mobile-web-app-title" content="Uno Score" />
+          <meta name="application-name" content="Uno Score" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
