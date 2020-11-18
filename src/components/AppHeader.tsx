6b321f2711e63ec523d54223ac1a5bef9b7c1784
miyamae <yu1ro.mya@gmail.com>
2020-11-18 16:29:56 +0900

    test commit

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -11,7 +11,9 @@ export const AppHeader: React.FC<Props> = ({ className }) => {
     <header className={clsx('p-4', className)}>
       <div className="flex justify-center items-center space-x-4">
         <Image src="/img/draw4.png" width={25} height={40} className="shadow" />
-        <h1 className="text-2xl font-bold italic text-gray-800">UNO Score</h1>
+        <h1 className="text-2xl font-bold italic text-gray-800">
+          UNO Score!!!
+        </h1>
       </div>
     </header>
   )
