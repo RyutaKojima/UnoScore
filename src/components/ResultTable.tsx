45ea3ff62c12f6fd094463c25db0d0acc930673c
r-kojima <rkojima0213@gmail.com>
2021-02-12 00:23:13 +0900

    チャート表示を追加

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -1,18 +1,16 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { TableCell } from './TableCell'
 import { Label } from './Label'
-import { IResult } from '../interfaces/result'
+import { ResultRow } from '../interfaces/result'
 import { ResultTableFooter } from './ResultTableFooter'
 import { IOption } from '../interfaces/option'
 import clsx from 'clsx'
 
 type Props = {
   players: string[]
-  rounds: number[][]
   options: IOption
+  results: ResultRow[]
 }
-
-type ResultRow = IResult[]
 
 const getRankColor = (rank: number): string => {
   return clsx('font-bold', {
@@ -22,54 +20,8 @@ const getRankColor = (rank: number): string => {
   })
 }
 
-const getFinalScore = (
-  rank: number,
-  rawScore: number,
-  options: IOption
-): number => {
-  if (rank === 2 && options.rescueSecond) {
-    return 0
-  }
-  if (rank === 3 && options.rescueThird) {
-    return 0
-  }
-
-  return rawScore
-}
-
 export const ResultTable = (props: Props): JSX.Element => {
-  const results = useMemo(() => {
-    let prevRound: IResult[] = []
-    return props.rounds.map((round) => {
-      const ranking: ResultRow = round.map((v) => ({
-        rank: round.filter((target) => v > target).length + 1,
-        score: v,
-        total: 0,
-      }))
-
-      const rescued: ResultRow = ranking.map((v) => ({
-        ...v,
-        score: getFinalScore(v.rank, v.score, props.options),
-      }))
-
-      const roundScoreTotal: number = rescued.reduce(
-        (prev, current) => prev + current.score,
-        0
-      )
-
-      const fixCurrentRound = rescued.map((v, index) => {
-        const score = v.rank === 1 ? roundScoreTotal : v.score * -1
-        return {
-          ...v,
-          score,
-          total: score + (prevRound[index]?.total ?? 0),
-        }
-      })
-
-      prevRound = fixCurrentRound
-      return fixCurrentRound
-    })
-  }, [props.rounds, props.options])
+  const results = props.results
 
   return (
     <table className="table-fixed w-full">
